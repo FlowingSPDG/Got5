5b31f06e6f85fbb69c281a8840a04701dfc26423
Shugo Kawamura <shugo_k@flowing.tokyo>
2022-11-20 17:14:44 +0900

    Deleted EventHandler auth, added test

--- route/events.go
+++ route/events.go
@@ -26,247 +26,279 @@ func OnEventHandler(ctrl controller.EventHandler) func(c *fiber.Ctx) error {
 	return (func(c *fiber.Ctx) error {
 		p := make(map[string]any)
 		if err := c.BodyParser(&p); err != nil {
-			return err
+			c.SendString(err.Error()) // TODO: Wrap error code
+			return c.SendStatus(fiber.StatusBadRequest)
 		}
 		ev, ok := (p["event"]).(string)
 		if !ok {
-			return fmt.Errorf("Unsupported JSON Format")
+			c.SendString("Invalid JSON")
+			return c.SendStatus(fiber.StatusBadRequest)
 		}
 		switch ev {
 		case "game_state_changed":
 			ret := models.OnGameStateChangedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnGameStateChanged(c.Context(), ret)
 		case "preload_match_config":
 			ret := models.OnPreLoadMatchConfigPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnPreLoadMatchConfig(c.Context(), ret)
 		case "match_config_load_fail":
 			ret := models.OnLoadMatchConfigFailedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnLoadMatchConfigFailed(c.Context(), ret)
 		case "series_start":
 			ret := models.OnSeriesInitPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnSeriesInit(c.Context(), ret)
 		case "map_result":
 			ret := models.OnMapResultPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnMapResult(c.Context(), ret)
 		case "series_end":
 			ret := models.OnSeriesResultPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnSeriesResult(c.Context(), ret)
 		case "side_picked":
 			ret := models.OnSidePickedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnSidePicked(c.Context(), ret)
 		case "map_picked":
 			ret := models.OnMapPickedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnMapPicked(c.Context(), ret)
 		case "map_vetoed":
 			ret := models.OnMapVetoedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnMapVetoed(c.Context(), ret)
 		case "backup_loaded":
 			ret := models.OnBackupRestorePayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnBackupRestore(c.Context(), ret)
 		case "demo_finished":
 			ret := models.OnDemoFinishedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnDemoFinished(c.Context(), ret)
 		case "demo_upload_ended":
 			ret := models.OnDemoUploadEndedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnDemoUploadEnded(c.Context(), ret)
 		case "game_paused":
 			ret := models.OnMatchPausedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnMatchPaused(c.Context(), ret)
 		case "game_unpaused":
 			ret := models.OnMatchUnpausedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnMatchUnpaused(c.Context(), ret)
 		case "knife_start":
 			ret := models.OnKnifeRoundStartedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnKnifeRoundStarted(c.Context(), ret)
 		case "knife_won":
 			ret := models.OnKnifeRoundWonPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnKnifeRoundWon(c.Context(), ret)
 		case "team_ready_status_changed":
 			ret := models.OnTeamReadyStatusChangedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnTeamReadyStatusChanged(c.Context(), ret)
 		case "going_live":
 			ret := models.OnGoingLivePayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnGoingLive(c.Context(), ret)
 		case "round_start":
 			ret := models.OnRoundStartPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnRoundStart(c.Context(), ret)
 		case "round_end":
 			ret := models.OnRoundEndPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnRoundEnd(c.Context(), ret)
 		case "stats_updated":
 			ret := models.OnRoundStatsUpdatedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnRoundStatsUpdated(c.Context(), ret)
 		case "round_mvp":
 			ret := models.OnPlayerBecameMVPPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnPlayerBecameMVP(c.Context(), ret)
 		case "grenade_thrown":
 			ret := models.OnGrenadeThrownPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnGrenadeThrown(c.Context(), ret)
 		case "player_death":
 			ret := models.OnPlayerDeathPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnPlayerDeath(c.Context(), ret)
 		case "hegrenade_detonated":
 			ret := models.OnHEGrenadeDetonatedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnHEGrenadeDetonated(c.Context(), ret)
 		case "molotov_detonated":
 			ret := models.OnMolotovDetonatedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnMolotovDetonated(c.Context(), ret)
 		case "flashbang_detonated":
 			ret := models.OnFlashbangDetonatedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnFlashbangDetonated(c.Context(), ret)
 		case "smokegrenade_detonated":
 			ret := models.OnSmokeGrenadeDetonatedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnSmokeGrenadeDetonated(c.Context(), ret)
 		case "decoygrenade_started":
 			ret := models.OnDecoyStartedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnDecoyStarted(c.Context(), ret)
 		case "bomb_planted":
 			ret := models.OnBombPlantedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnBombPlanted(c.Context(), ret)
 		case "bomb_defused":
 			ret := models.OnBombDefusedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnBombDefused(c.Context(), ret)
 		case "bomb_exploded":
 			ret := models.OnBombExplodedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnBombExploded(c.Context(), ret)
 		case "player_connect":
 			ret := models.OnPlayerConnectedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnPlayerConnected(c.Context(), ret)
 		case "player_disconnect":
 			ret := models.OnPlayerDisconnectedPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnPlayerDisconnected(c.Context(), ret)
 		case "player_say":
 			ret := models.OnPlayerSayPayload{}
 			if err := reMarshal(p, &ret); err != nil {
-				return err
+				c.SendString(fmt.Sprintf("Invalid JSON:%s", err.Error()))
+				return c.SendStatus(fiber.StatusBadRequest)
 			}
 			return ctrl.HandleOnPlayerSay(c.Context(), ret)
 		default:
-			return fmt.Errorf("Event Not found%s", ev)
+			c.SendString(fmt.Sprintf("Invalid Event"))
+			return c.SendStatus(fiber.StatusBadRequest)
 		}
 	})
 }
 
-// CheckEventHandlerAuth 認証用ハンドラ
+// CheckEventHandlerAuth 認証用ハンドラ Under construction, this middleware is not working now.
 func CheckEventHandlerAuth(ctrl controller.EventHandler) func(c *fiber.Ctx) error {
 	return (func(c *fiber.Ctx) error {
-		p := make(map[string]any)
-		if err := c.BodyParser(&p); err != nil {
-			return err
-		}
-		mid, ok := (p["matchid"]).(string)
-		if !ok {
-			return fmt.Errorf("Unsupported JSON Format")
-		}
-
-		reqAuthVal := c.Get("Authorization")
-
-		if err := ctrl.CheckEventAuth(c.Context(), mid, reqAuthVal); err != nil {
-			c.Status(fiber.StatusUnauthorized)
-			return nil
-		}
+		// TODO: マッチIDはどう取得する？
+		// get5_remote_log_header_key をマッチIDにするしかないかもしれない
+		// reqAuthVal := c.Get("Authorization")
+		/*
+			if err := ctrl.CheckEventAuth(c.Context(), mid, reqAuthVal); err != nil {
+				c.Status(fiber.StatusUnauthorized)
+				return nil
+			}
+		*/
 		return c.Next()
 	})
 }
---
+++ route/events_test.go
@@ -0,0 +1,318 @@
+package route_test
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/FlowingSPDG/Got5/controller"
+	"github.com/FlowingSPDG/Got5/models"
+	"github.com/FlowingSPDG/Got5/route"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+var _ controller.EventHandler = (*mockEventHandler)(nil)
+
+type mockEventHandler struct {
+	expect any
+	parsed any
+}
+
+// CheckEventAuth implements controller.EventHandler
+func (m *mockEventHandler) CheckEventAuth(ctx context.Context, mid string, reqAuth string) error {
+	return nil
+}
+
+// Close implements controller.EventHandler
+func (m *mockEventHandler) Close() error {
+	return nil
+}
+
+// HandleOnBackupRestore implements controller.EventHandler
+func (m *mockEventHandler) HandleOnBackupRestore(ctx context.Context, p models.OnBackupRestorePayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnBombDefused implements controller.EventHandler
+func (m *mockEventHandler) HandleOnBombDefused(ctx context.Context, p models.OnBombDefusedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnBombExploded implements controller.EventHandler
+func (m *mockEventHandler) HandleOnBombExploded(ctx context.Context, p models.OnBombExplodedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnBombPlanted implements controller.EventHandler
+func (m *mockEventHandler) HandleOnBombPlanted(ctx context.Context, p models.OnBombPlantedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnDecoyStarted implements controller.EventHandler
+func (m *mockEventHandler) HandleOnDecoyStarted(ctx context.Context, p models.OnDecoyStartedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnDemoFinished implements controller.EventHandler
+func (m *mockEventHandler) HandleOnDemoFinished(ctx context.Context, p models.OnDemoFinishedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnDemoUploadEnded implements controller.EventHandler
+func (m *mockEventHandler) HandleOnDemoUploadEnded(ctx context.Context, p models.OnDemoUploadEndedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnFlashbangDetonated implements controller.EventHandler
+func (m *mockEventHandler) HandleOnFlashbangDetonated(ctx context.Context, p models.OnFlashbangDetonatedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnGameStateChanged implements controller.EventHandler
+func (m *mockEventHandler) HandleOnGameStateChanged(ctx context.Context, p models.OnGameStateChangedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnGoingLive implements controller.EventHandler
+func (m *mockEventHandler) HandleOnGoingLive(ctx context.Context, p models.OnGoingLivePayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnGrenadeThrown implements controller.EventHandler
+func (m *mockEventHandler) HandleOnGrenadeThrown(ctx context.Context, p models.OnGrenadeThrownPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnHEGrenadeDetonated implements controller.EventHandler
+func (m *mockEventHandler) HandleOnHEGrenadeDetonated(ctx context.Context, p models.OnHEGrenadeDetonatedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnKnifeRoundStarted implements controller.EventHandler
+func (m *mockEventHandler) HandleOnKnifeRoundStarted(ctx context.Context, p models.OnKnifeRoundStartedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnKnifeRoundWon implements controller.EventHandler
+func (m *mockEventHandler) HandleOnKnifeRoundWon(ctx context.Context, p models.OnKnifeRoundWonPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnLoadMatchConfigFailed implements controller.EventHandler
+func (m *mockEventHandler) HandleOnLoadMatchConfigFailed(ctx context.Context, p models.OnLoadMatchConfigFailedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnMapPicked implements controller.EventHandler
+func (m *mockEventHandler) HandleOnMapPicked(ctx context.Context, p models.OnMapPickedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnMapResult implements controller.EventHandler
+func (m *mockEventHandler) HandleOnMapResult(ctx context.Context, p models.OnMapResultPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnMapVetoed implements controller.EventHandler
+func (m *mockEventHandler) HandleOnMapVetoed(ctx context.Context, p models.OnMapVetoedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnMatchPaused implements controller.EventHandler
+func (m *mockEventHandler) HandleOnMatchPaused(ctx context.Context, p models.OnMatchPausedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnMatchUnpaused implements controller.EventHandler
+func (m *mockEventHandler) HandleOnMatchUnpaused(ctx context.Context, p models.OnMatchUnpausedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnMolotovDetonated implements controller.EventHandler
+func (m *mockEventHandler) HandleOnMolotovDetonated(ctx context.Context, p models.OnMolotovDetonatedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnPlayerBecameMVP implements controller.EventHandler
+func (m *mockEventHandler) HandleOnPlayerBecameMVP(ctx context.Context, p models.OnPlayerBecameMVPPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnPlayerConnected implements controller.EventHandler
+func (m *mockEventHandler) HandleOnPlayerConnected(ctx context.Context, p models.OnPlayerConnectedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnPlayerDeath implements controller.EventHandler
+func (m *mockEventHandler) HandleOnPlayerDeath(ctx context.Context, p models.OnPlayerDeathPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnPlayerDisconnected implements controller.EventHandler
+func (m *mockEventHandler) HandleOnPlayerDisconnected(ctx context.Context, p models.OnPlayerDisconnectedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnPlayerSay implements controller.EventHandler
+func (m *mockEventHandler) HandleOnPlayerSay(ctx context.Context, p models.OnPlayerSayPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnPreLoadMatchConfig implements controller.EventHandler
+func (m *mockEventHandler) HandleOnPreLoadMatchConfig(ctx context.Context, p models.OnPreLoadMatchConfigPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnRoundEnd implements controller.EventHandler
+func (m *mockEventHandler) HandleOnRoundEnd(ctx context.Context, p models.OnRoundEndPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnRoundStart implements controller.EventHandler
+func (m *mockEventHandler) HandleOnRoundStart(ctx context.Context, p models.OnRoundStartPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnRoundStatsUpdated implements controller.EventHandler
+func (m *mockEventHandler) HandleOnRoundStatsUpdated(ctx context.Context, p models.OnRoundStatsUpdatedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnSeriesInit implements controller.EventHandler
+func (m *mockEventHandler) HandleOnSeriesInit(ctx context.Context, p models.OnSeriesInitPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnSeriesResult implements controller.EventHandler
+func (m *mockEventHandler) HandleOnSeriesResult(ctx context.Context, p models.OnSeriesResultPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnSidePicked implements controller.EventHandler
+func (m *mockEventHandler) HandleOnSidePicked(ctx context.Context, p models.OnSidePickedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnSmokeGrenadeDetonated implements controller.EventHandler
+func (m *mockEventHandler) HandleOnSmokeGrenadeDetonated(ctx context.Context, p models.OnSmokeGrenadeDetonatedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// HandleOnTeamReadyStatusChanged implements controller.EventHandler
+func (m *mockEventHandler) HandleOnTeamReadyStatusChanged(ctx context.Context, p models.OnTeamReadyStatusChangedPayload) error {
+	m.parsed = p
+	return nil
+}
+
+// Hostname implements controller.EventHandler
+func (m *mockEventHandler) Hostname() string {
+	return ""
+}
+
+func TestEventHandleTD(t *testing.T) {
+	asserts := assert.New(t)
+
+	cases := []struct {
+		title        string
+		eventHandler *mockEventHandler
+		statusCode   int
+		input        []byte
+		// err        error
+	}{
+		{
+			title: "Example Invalid Event",
+			eventHandler: &mockEventHandler{
+				expect: nil,
+			},
+			statusCode: http.StatusBadRequest,
+			input: []byte(`{
+				"event": "string"
+			  }`),
+			// err:        nil,
+		},
+		{
+			title: "GameStateChanged",
+			eventHandler: &mockEventHandler{
+				expect: models.OnGameStateChangedPayload{
+					Event:    models.Event{Event: "game_state_changed"},
+					NewState: "none",
+					OldState: "none",
+				},
+			},
+			statusCode: http.StatusOK,
+			// err:        nil,
+			input: []byte(`{"event": "game_state_changed","new_state": "none","old_state": "none"}`),
+		},
+		{
+			title: "PreloadMatchConfig",
+			eventHandler: &mockEventHandler{
+				expect: models.OnPreLoadMatchConfigPayload{
+					Event:    models.Event{Event: "preload_match_config"},
+					Filename: "TEST_FILENAME",
+				},
+			},
+			statusCode: http.StatusOK,
+			// err:        nil,
+			input: []byte(`{"event": "preload_match_config","filename": "TEST_FILENAME"}`),
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.title, func(t *testing.T) {
+			// Setup fiber
+			app := fiber.New()
+			g5test := app.Group("/get5testevent") // /test
+			err := route.SetupEventHandlers(tt.eventHandler, g5test)
+			asserts.NoError(err)
+
+			r := httptest.NewRequest("POST", "/get5testevent/event", bytes.NewBuffer(tt.input))
+			r.Header.Set("Content-Type", "application/json")
+
+			resp, _ := app.Test(r, -1)
+			defer resp.Body.Close()
+			b, _ := io.ReadAll(resp.Body)
+			t.Logf("b:%s\n", b)
+			// asserts.Equal(tt.err, err)
+			asserts.Equal(tt.statusCode, resp.StatusCode)
+			asserts.Equal(tt.eventHandler.parsed, tt.eventHandler.parsed)
+		})
+	}
+}
